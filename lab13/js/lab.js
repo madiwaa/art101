8fb3ee362a0ca6c590e39e5106c820406c2b5583
madiwaa <madisonanav@gmail.com>
2024-11-18 19:07:40 -0800

    lab 13 incomplete
    I need to finish the js, just to submit so its not late

---
+++ lab13/js/lab.js
@@ -0,0 +1,11 @@
+/*
+   Task: Working with your partner, create a function that depends on conditionals.
+   Maker: Madison Navarro
+   Date: 18 November 2024
+*/
+
+maxFactors = 4;
+
+outputEl = document.getElementById("output");
+
+//get values and turn them into an object
